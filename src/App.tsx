58c55b4beb6b721901ec2a3c30866a81b436de1b
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-08-15 18:00:14 -0300

    Estilos globais | Cores & Fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
 
@@ -10,6 +11,9 @@ export function App() {
       <Button variant='secondary'/>
       <Button variant='danger'/>
       <Button variant='success'/>
+
+
+      <GlobalStyle />
     </ThemeProvider >
   )
 }
