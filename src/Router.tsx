39f849369d9da0ed775f3ffc4fad08e0b479c5fd
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-08-16 16:23:29 -0300

    Layout de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
+import { DefaultLayout } from "./layouts/DefaultLayout";
 import { History } from "./pages/History";
 import { Home } from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 
 export function Router() {
-    return (
-       <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/History" element={<History />}/>
-       </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/History" element={<History />} />
+      </Route>
+    </Routes>
+  );
 }
