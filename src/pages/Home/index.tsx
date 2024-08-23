05f88d57e5b03950e0e815ddf5d0b921bf0034cf
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-08-23 16:42:59 -0300

    Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,38 @@
 import { Play } from "phosphor-react";
-import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from "./styles";
 
 export function Home() {
   return (
     <HomeContainer>
       <form action="">
         <FormContainer>
+
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto"/>
+          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1"/>
+            <option value="Projeto 2"/>
+            <option value="Projeto 3"/>
+          </datalist>
+
           <label htmlFor="minutesAmount">Durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00"/>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
           <span>minutos</span>
         </FormContainer>
 
@@ -24,7 +47,7 @@ export function Home() {
         <StartCountDownButton disabled type="submit">
           <Play />
           Começar
-        </StartCountDownButton >
+        </StartCountDownButton>
       </form>
     </HomeContainer>
   );
