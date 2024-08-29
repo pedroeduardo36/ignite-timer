d750e598a33c194396178b429ca4db84421436e3
pedroeduardo36 <pedrovilarinho.gpd@gmail.com>
2024-08-29 17:55:04 -0300

    Valindo formulários & Feat zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,17 +9,28 @@ import {
   TaskInput,
 } from "./styles";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
+    .max(60, "O ciclo pode ser de no máximo 60 minutos."),
+});
 
 export function Home() {
-  const { register, handleSubmit, watch} = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
 
   function handleCreateNewCycle(data) {
     console.log(data);
   }
 
-  const task = watch('task')
-  const isSubmitDisabled = !task
-
+  const task = watch("task");
+  const isSubmitDisabled = !task;
 
   return (
     <HomeContainer>
@@ -30,7 +41,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
-            {...register('task')}
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -47,7 +58,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
-            {...register('minutesAmount', {valueAsNumber: true})}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
           <span>minutos</span>
         </FormContainer>
